af4677cf6d66b5f3a6f5e48e8afc3c84552ccbd7
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2021-11-30 02:15:23 -0300

    Add files via upload

---
+++ basic/math-operations.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+// Declarar a versão do programa
+
+pragma solidity ^0.8.10;
+
+// As operações matemáticas em solidity é como na maioria das linguagens
+
+// Adição: x + y
+// Subtração: x - y
+// Multiplicação: x * y
+// Divisão: x / y
+// Módulo da divisão: x % y
+// Exponencial: x ** y (lê-se x elevado a y)
---
+++ basic/state-variables.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+// Declara a versão do programa
+
+pragma solidity ^0.8.10;
+
+// Variáveis de estado e inteiros
+
+contract Exemplo {
+    uint numero = 100;
+}
+
+// As variáveis de estado são permanentemente armazenadas no contrato. Isto significa que elas são escritas na blockchain do Ethereum. É como se você estivesse gravando essas variáveis em um banco de dados.
+// uint - significa unsigned integer - ou seja - seu valor não pode ser negativo
